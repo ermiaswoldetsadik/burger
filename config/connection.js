52a1360775520a262a7f678f691c169109c0f9ea
ermias <ermiaswoldetsadik@gmail.com>
2020-01-05 11:18:41 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
 // dependencies
 var mysql = require("mysql");
-
-	var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+	connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
 	user: "root",
@@ -9,7 +12,7 @@ var mysql = require("mysql");
 	database: "burgers_db"
 
 	});
-	
+};
 
 connection.connect(function(err) {
 	if (err) {
